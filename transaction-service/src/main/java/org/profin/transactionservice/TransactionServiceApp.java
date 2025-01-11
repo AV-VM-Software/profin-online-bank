8c1020ab4f0967a28c4d84ab0cd8ca16ce12ae52
Artem Vozhov <vozhov.artem1@gmail.com>
2025-01-11 16:21:34 +0100

    start spring boot main

--- transaction-service/src/main/java/org/profin/transactionservice/TransactionServiceApp.java
+++ transaction-service/src/main/java/org/profin/transactionservice/TransactionServiceApp.java
@@ -1,13 +1,14 @@
 package org.profin.transactionservice;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransactionServiceApp
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionServiceApp.class, args);
     }
+
 }
