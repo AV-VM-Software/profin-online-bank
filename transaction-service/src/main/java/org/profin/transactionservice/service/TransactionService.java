913559d4465d37598341e1b85f940eba44d1b947
Artem Vozhov <vozhov.artem1@gmail.com>
2025-01-11 17:00:52 +0100

    reactive save

--- transaction-service/src/main/java/org/profin/transactionservice/service/TransactionService.java
+++ transaction-service/src/main/java/org/profin/transactionservice/service/TransactionService.java
@@ -2,6 +2,7 @@ package org.profin.transactionservice.service;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.profin.transactionservice.entity.Currency;
 import org.profin.transactionservice.entity.PaymentStatus;
 import org.profin.transactionservice.entity.Transaction;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
@@ -23,17 +25,22 @@ public class TransactionService {
 
     //todo use dto or map from dto on controller level
     private Mono<Transaction> saveTransaction(Transaction transaction) {
-        return Mono.fromCallable(() -> {
-            //save to db
-            transactionRepository.save(transaction);
-            return transaction;
-        });
+        log.debug("TransactionService: Saving transaction: {}", transaction);
+        return transactionRepository.save(transaction).doOnSuccess(
+                savedTransacation -> log.debug("TransactionService: Transaction saved: {}", savedTransacation)
+        ).doOnError(
+                throwable ->
+                        log.error("TransactionService: Error saving transaction: {}", throwable.getMessage())
+
+        );
     }
 
 
 
+
+
     //dev mode
-    private Transaction buildTransefTransaction() {
+    public Transaction buildTransefTransaction() {
         return new Transaction().builder().userId(1L).
         recipientId(2L).
         amount(BigDecimal.valueOf(100.0)).
