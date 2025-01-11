661151850e8d94f68563acc1cbc479d1eb382b97
Artem Vozhov <vozhov.artem1@gmail.com>
2025-01-11 16:55:20 +0100

    need to use R2DBC

--- transaction-service/src/main/java/org/profin/transactionservice/repository/TransactionRepository.java
+++ transaction-service/src/main/java/org/profin/transactionservice/repository/TransactionRepository.java
@@ -1,9 +1,10 @@
 package org.profin.transactionservice.repository;
 
 import org.profin.transactionservice.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+public interface TransactionRepository extends ReactiveCrudRepository<Transaction,Long> {
 }
