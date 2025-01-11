aaeb92d6b1cd6c5c9a10753b17c5c63c0274e317
Vitalii Manilnykov <manilvit@fel.cvut.cz>
2025-01-11 12:28:22 +0100

    test

---
+++ UserService/src/main/java/org/example/dto/UserMapper.java
@@ -0,0 +1,24 @@
+package org.example.dto;
+
+import org.example.model.User;
+
+public class UserMapper {
+
+    public UserDTO toDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+
+        return userDTO;
+    }
+
+    public User toEntity(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+
+        return user;
+    }
+}
---
+++ UserService/src/main/java/org/example/model/BankAccount.java
@@ -0,0 +1,52 @@
+package org.example.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+}
